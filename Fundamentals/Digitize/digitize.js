cfca8822d23ebfef18d127b4ce673af9b11593f9
yoel <67129044+yoel0@users.noreply.github.com>
2020-11-06 17:27:18 -0800

    [add] Fundamentals, Digitize

---
+++ Fundamentals/Digitize/digitize.js
@@ -0,0 +1,26 @@
+/* Digitize, Javascript, Math
+* Converts a Number to an Array of digits,
+* removing its sign if necessary.
+*
+* 1. Use Math.abs() to strip the number's sign.
+* 2. Convert the number to a String, using the Spread Operator (...)
+* to build an Array.
+* 3. Use Array.prototype.map() and parseInt() to transform,
+* each value to an integer.
+*/
+
+const spitDigits = n => [...`${Math.abs(n)}`].map(i =>
+  parseInt(i));
+
+  console.log(spitDigits(123456789));
+  console.log(spitDigits(-123456789));
+
+/* returns [
+  1, 2, 3, 4, 5,
+  6, 7, 8, 9
+]
+[
+  1, 2, 3, 4, 5,
+  6, 7, 8, 9
+]
+*/
