3d8544e4bfb79a4f7783d2aa6db3c05434a487a5
yoel <67129044+yoel0@users.noreply.github.com>
2020-11-18 00:40:40 -0800

    [add] Fundamentals, dayName

---
+++ Fundamentals/dayName/dayName.js
@@ -0,0 +1,26 @@
+/* dayName, Javascript, Date
+* Gets the name of the weekday from a Date object.
+*
+* 1. Use Date.prototype.toLocaleDateString() with 
+* the { weekday: 'long' } option to retrieve the weekday.
+* 2. Use the optional second argument to get a language-specific name 
+* or omit it to use the default locale.
+*/
+
+const lullabyOfDays = (date, locale) =>
+  date.toLocaleDateString(locale, { weekday: 'long'});
+
+console.log(lullabyOfDays(new Date())); // default locale
+console.log(lullabyOfDays(new Date('11/18/2020'), 'de-DE')); // Deutsche
+console.log(lullabyOfDays(new Date('11/18/2020'), 'hi-IN')); // Hindi Indian
+console.log(lullabyOfDays(new Date('11/18/2020'), 'ko-KR')); // Korean
+console.log(lullabyOfDays(new Date('11/18/2020'), 'zh-CN')); // Simplified Chinese
+
+// Returns
+// Wednesday
+// Mittwoch
+// बुधवार
+// 수요일
+// 星期三
+
+
