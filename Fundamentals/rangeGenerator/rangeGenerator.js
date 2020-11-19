8cb6c6d3cb6569d78ed7ee27891f25309d283c4a
yoel <67129044+yoel0@users.noreply.github.com>
2020-11-19 00:24:58 -0800

    [add] Fundamentals, rangeGenerator

---
+++ Fundamentals/rangeGenerator/rangeGenerator.js
@@ -0,0 +1,19 @@
+/* rangeGenerator, Javascript, Function, Generator
+* Creates a generator, that generates all values 
+* in the given range using the given step.
+*
+* 1. Use a while loop to iterate from start to end, 
+* using yield to return each value and then incrementing by step.
+* 2. Omit the third argument, step, to use a default value of 1.
+*/
+
+const invokeRange = function* (creation, destruction, growth = 1) {
+  let i = creation;
+  while (i < destruction) {
+    yield i;
+    i += growth;
+  }
+};
+
+for (let i of invokeRange(8, 26))
+console.log('%c waz my range', 'background: orange; color: white; display: block;', i);
