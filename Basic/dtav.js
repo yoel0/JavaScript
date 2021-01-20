4091ea04ce9356da03469e18bf766287c5e800b7
yoel <67129044+yoel0@users.noreply.github.com>
2021-01-20 15:16:40 -0800

    [add] Datatypes Notes

---
+++ Basic/dtav.js
@@ -0,0 +1,29 @@
+// Datatypes
+//  6 Primitive Datatypes
+// 1. undefined
+// 2. null
+// 3. Boolean
+// 4. Number
+// 5. String
+// 6. Symbol
+//  1 Reference Datatype
+// 1. Object
+
+// Total = 7 Datatypes
+
+/* In JavaScript 
+Primitive values are data that are stored on the stack.
+Primitive value is stored directly in the location that the variable accesses.
+
+Reference values are objects that are stored in the heap.
+Referece value stored in the variable location is a pointer
+to a location in memory where object is stored.
+
+Basics: 
+Objects are aggregations of properties. A property can reference an object or a primitive.
+Primitives are values, they have no properties.
+
+With the exception of null and undefined, all primitves values have object equivalents,
+which wrap around the primitive values, e.g. a String object wraps around a string prmitive.
+All primitives are immutable.
+*/
