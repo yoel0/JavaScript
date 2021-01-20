9f2e1e6a14546439614089f8b0acb5fd4007facd
yoel <67129044+yoel0@users.noreply.github.com>
2021-01-20 14:24:21 -0800

    [add] Basic, document getElement, write and ID

--- Basic/script.js
+++ Basic/script.js
@@ -1,6 +1,12 @@
-// Write to the document.
-document.write("<h1>Goodbye, World!</h1>");
+// Write to the HTML document.
+document.write("<h1>Hello, World!</h1>");
 // Console log, warn and error.
 console.log("Hello, this is a log.");
 console.warn("This is a Warning!");
 console.error("This is an Error!!");
+
+// Manipulate the HTML document.
+// Get the value / parameter "header".
+// Display to the Inner HTML at "header".
+// Write to the tag/id by using document.write().
+document.write(document.getElementById("header").innerHTML = "I am back again, World.");
