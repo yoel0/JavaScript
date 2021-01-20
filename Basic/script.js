11aab55515ed546dd73a1cb325ed88541201c3ca
yoel <67129044+yoel0@users.noreply.github.com>
2021-01-20 14:32:03 -0800

    [add] Basic, Input Box, value change

--- Basic/script.js
+++ Basic/script.js
@@ -1,5 +1,5 @@
 // Write to the HTML document.
-document.write("<h1>Hello, World!</h1>");
+document.write("<h1>Goodbye, World!</h1>");
 // Console log, warn and error.
 console.log("Hello, this is a log.");
 console.warn("This is a Warning!");
@@ -10,3 +10,7 @@ console.error("This is an Error!!");
 // Display to the Inner HTML at "header".
 // Write to the tag/id by using document.write().
 document.write(document.getElementById("header").innerHTML = "I am back again, World.");
+
+// Get the Element by id from the input box.
+// Change the value of the input box to Schnitzel.
+document.getElementById("inp").value = "Schnitzel";
