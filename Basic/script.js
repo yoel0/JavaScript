94d2b5a2319ac2c7b2311d278b7be84f447d362f
yoel <67129044+yoel0@users.noreply.github.com>
2021-01-20 14:15:08 -0800

    [add] Basic, comments

--- Basic/script.js
+++ Basic/script.js
@@ -1,4 +1,6 @@
+// Write to the document.
 document.write("<h1>Goodbye, World!</h1>");
+// Console log, warn and error.
 console.log("Hello, this is a log.");
 console.warn("This is a Warning!");
 console.error("This is an Error!!");
