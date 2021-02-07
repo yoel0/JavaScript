1a96ca9f834ac581b3f6b076cc738283fa3fb4a0
yoel <67129044+yoel0@users.noreply.github.com>
2021-02-06 23:47:20 -0800

    [add] Fundamentals, sumN

---
+++ Fundamentals/sumN/sumN.js
@@ -0,0 +1,13 @@
+/* sumN, Javascript, Math
+* Sums all the numbers between 1 and n.
+*
+* 1. Use the formula (n * (n + 1)) / 2 
+* to get the sum of all the numbers between 1 and n.
+*/
+
+const sumN = n => (n * (n + 1)) / 2;
+
+console.log('Sum ma cherieee: ', sumN(100));
+
+// Returns
+// Sum ma cherieee:  5050
