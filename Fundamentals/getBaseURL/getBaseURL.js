69cd8ce6efe78729b114eca31ec21deb611367d9
yoel <67129044+yoel0@users.noreply.github.com>
2021-02-12 00:03:18 -0800

    [edit] Shorten getBaseURL

--- Fundamentals/getBaseURL/getBaseURL.js
+++ Fundamentals/getBaseURL/getBaseURL.js
@@ -8,7 +8,7 @@
 const getBaseURL = url => url.replace(/[?#].*$/, '');
 
 console.log(getBaseURL(
-  'https://babeljs.io/repl/#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011&build=&builtIns=false&spec=false&loose=false&code_lz=MYewdgzgLgBApgGzgWzmWBeGAeAFgRgD4AJRBEAGhgHcQAnBAEwEJsB6AwgbgChRJY_KAEMAlmDh0YWRiGABXVOgB0AczhQAokiVQAQgE8AkowAUAcjogQUcwEpeAJTjDgUACIB5ALLK6aRklTRBQ0KCohMQk6Bx4gA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=react&prettier=false&targets=&version=7.12.15&externalPlugins='
+  'https://babeljs.io/repl/#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011'
 ));
 
 // Returns
