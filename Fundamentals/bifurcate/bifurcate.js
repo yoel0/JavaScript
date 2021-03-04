12bc993e85e6de878d5a2de7747101ca2721b148
yoel <67129044+yoel0@users.noreply.github.com>
2021-03-03 23:09:38 -0800

    [add] Fundamentals, bifurcate

---
+++ Fundamentals/bifurcate/bifurcate.js
@@ -0,0 +1,17 @@
+/* bifurcate, Javascript, Array
+* Splits values into two groups, based on the result 
+* of the given filter array.
+*
+* 1. Use Array.prototype.reduce() and Array.prototype.push() 
+* to add elements to groups, based on filter.
+* 2. If filter has a truthy value for any element, add it to 
+* the first group, otherwise add it to the second group.
+*/
+
+const bifurcate = (a, filter) =>
+  a.reduce((acc, val, i) => (acc[filter[i] ? 0 : 1].push(val), acc), [
+    [],
+    [],
+  ]);
+
+console.log(bifurcate(['one', 'two', 'four', 'three'], [true, true, false, true]));
