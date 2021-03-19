82b134cdc8bc25eee61d81738cc4e4c628f4c895
yoel <67129044+yoel0@users.noreply.github.com>
2021-03-19 08:01:55 -0700

    [add] Fundamentals, allEqual

---
+++ Fundamentals/allEqual/allEqual.js
@@ -0,0 +1,17 @@
+/* addCEqual, Javascript, Array
+ * Checks if all elements in an array are equal.
+ *
+ * 1. Use Array.prototype.every() to check if all the elements of
+ * the array are the same as the first one.
+ * 2. Elements in the array are compared using the strict comparison operator,
+ * which does not account for NaN self-inequality.
+ */
+
+const allEqual = array => array.every(value => value === array[0]);
+
+console.log(allEqual([1, 2, 3, 4, 5]));
+console.log(allEqual([1, 1, 1, 1, 1]));
+
+// Returns
+// false
+// true
