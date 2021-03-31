29c23111af920e950612d6df96b67d083505480c
yoel <67129044+yoel0@users.noreply.github.com>
2021-03-31 01:12:44 -0700

    [fix] space

--- Fundamentals/cartesianProduct/cartesianProduct.js
+++ Fundamentals/cartesianProduct/cartesianProduct.js
@@ -10,5 +10,6 @@ const cartesianProduct = (a, b) =>
   a.reduce((p, x) => [...p, ...b.map(y => [x, y])], []);
 
 console.log(cartesianProduct(['x', 'y'], [1, 2]));
+
 // Returns
 // [ [ 'x', 1 ], [ 'x', 2 ], [ 'y', 1 ], [ 'y', 2 ] ]
