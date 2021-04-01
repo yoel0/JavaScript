89713df14283b0a5b9ef6a1f8556f7afcfe0ddcf
yoel <67129044+yoel0@users.noreply.github.com>
2021-03-31 23:46:03 -0700

    [fix] space

--- Fundamentals/defer/defer.js
+++ Fundamentals/defer/defer.js
@@ -1,9 +1,9 @@
 /* defer, Javascript, Function
  * Defers invoking a function until the current call stack has cleared.
  *
- * 1. Use setTimeout() with a timeout of 1 ms to add a new event 
+ * 1. Use setTimeout() with a timeout of 1 ms to add a new event
  * to the event queue and allow the rendering engine to complete its work.
- * 2. Use the spread (...) operator to supply the function 
+ * 2. Use the spread (...) operator to supply the function
  * with an arbitrary number of arguments.
  */
 
